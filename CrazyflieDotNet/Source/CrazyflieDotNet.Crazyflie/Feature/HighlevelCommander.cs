8cde09f0cfb6c3d7d948f3168e8556ac8931cef6
Dominic Ullmann <dominic_ullmann@hotmail.com>
2018-10-15 06:22:36 +0200

    start to add high level commander

---
+++ CrazyflieDotNet/Source/CrazyflieDotNet.Crazyflie/Feature/HighlevelCommander.cs
@@ -0,0 +1,81 @@
+﻿using CrazyflieDotNet.CrazyMessaging;
+using CrazyflieDotNet.CrazyMessaging.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrazyflieDotNet.Crazyflie.Feature
+{
+    /// <summary>
+    /// Used for sending high level setpoints to the Crazyflie
+    /// </summary>
+    internal class HighlevelCommander : ICrazyflieHighlevelCommander
+    {
+
+        public const byte ALL_GROUPS = 0;
+
+        private enum HighlevelCommands : byte
+        {
+            COMMAND_SET_GROUP_MASK = 0,
+            COMMAND_TAKEOFF = 1,
+            COMMAND_LAND = 2,
+            COMMAND_STOP = 3,
+            COMMAND_GO_TO = 4,
+            COMMAND_START_TRAJECTORY = 5,
+            COMMAND_DEFINE_TRAJECTORY = 6,
+        }
+
+        private ICrtpCommunicator _communicator;
+
+        internal HighlevelCommander(ICrtpCommunicator communicator)
+        {
+            _communicator = communicator;
+        }
+
+        private void Send(MessageBuilder builder)
+        {
+            var message = builder.Build();
+            _communicator.SendMessage(message);
+        }
+
+        public void SetGroupMask(byte groupMask = 0)
+        {
+            byte command = (byte)HighlevelCommands.COMMAND_SET_GROUP_MASK;
+
+            var builder = new MessageBuilder(
+                (byte)CrtpPort.SETPOINT_HL, (byte)CrtpChannel.Channel0);
+            builder.Add(command);
+            builder.Add(groupMask);
+
+            Send(builder);
+        }
+
+        public void Takeoff(float absoluteHeightM, float durationInSec, byte groupMask = 0)
+        {
+            byte command = (byte)HighlevelCommands.COMMAND_TAKEOFF;
+
+            var builder = new MessageBuilder(
+                (byte)CrtpPort.SETPOINT_HL, (byte)CrtpChannel.Channel0);
+            builder.Add(command);
+            builder.Add(groupMask);            
+            builder.Add(absoluteHeightM);
+            builder.Add(durationInSec);
+
+            Send(builder);
+        }
+
+        public void Land(float absoluteHeightM, float durationInSec, byte groupMask = 0)
+        {
+            byte command = (byte)HighlevelCommands.COMMAND_LAND;
+
+            var builder = new MessageBuilder(
+                (byte)CrtpPort.SETPOINT_HL, (byte)CrtpChannel.Channel0);
+            builder.Add(command);
+            builder.Add(groupMask);
+            builder.Add(absoluteHeightM);
+            builder.Add(durationInSec);
+
+            Send(builder);
+        }
+    }
+}
---
+++ CrazyflieDotNet/Source/CrazyflieDotNet.Crazyflie/Feature/ICrazyflieHighlevelCommander.cs
@@ -0,0 +1,32 @@
+﻿namespace CrazyflieDotNet.Crazyflie.Feature
+{
+    /// <summary>
+    /// Allows to send high level commands to the crazyflie.
+    /// </summary>
+    public interface ICrazyflieHighlevelCommander
+    {
+
+        /// <summary>
+        /// Set the group mask that the Crazyflie belongs to
+        /// </summary>
+        /// <param name="groupMask">mask for which groups this CF belongs to</param>
+        void SetGroupMask(byte groupMask = HighlevelCommander.ALL_GROUPS);                
+
+        /// <summary>
+        /// vertical takeoff from current x-y position to given height
+        /// </summary>
+        /// <param name="absoluteHeightM"> absolut(m)</param>
+        /// <param name="durationInSec">time it should take until target height is reached(s)</param>
+        /// <param name="groupMask">mask for which CFs this should apply to</param>
+        void Takeoff(float absoluteHeightM, float durationInSec, byte groupMask = HighlevelCommander.ALL_GROUPS);
+
+        /// <summary>
+        /// vertical land from current x-y position to given height
+        /// </summary>
+        /// <param name="absoluteHeightM">absolut(m)</param>
+        /// <param name="durationInSec">time it should take until target height is reached(s)</param>
+        /// <param name="groupMask">mask for which CFs this should apply to</param>
+        void Land(float absoluteHeightM, float durationInSec, byte groupMask = HighlevelCommander.ALL_GROUPS);
+
+    }
+}
